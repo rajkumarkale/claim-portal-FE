7d2928ebda908420f5d7f5abe09b3b85757fe898
Rajkumar Kale <rajkumar.kale@mtuity.com>
2017-01-09 15:44:20 +0530

    first

--- app/scripts/components/employee/controllers/apply-claim-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kveena on 12/28/2016.
- */
-angular.module("claimPortalApp")
-  .controller("ApplyClaimController", ['$scope', '$state',function($scope, $state) {
-    var claimCtrl = this;
-    
-  }]);
--- app/scripts/components/employee/controllers/employee-dashboard-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kveena on 12/15/2016.
- */
-angular.module("claimPortalApp")
-  .controller("EmployeeDashboardCtrl", ['$scope', '$state',function($scope, $state){
-    var empDashboardCtrl=this;
-    
-}]);
